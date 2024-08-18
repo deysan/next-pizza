1f8640dec8bee15ec6acb9eee63c3400a77f81a7
deysan <ideysan@gmail.com>
2024-08-18 09:32:49 +0300

    Add filters

---
+++ components/shared/filters.tsx
@@ -0,0 +1,121 @@
+import { Input } from "@/components/ui/input";
+
+import { Title } from "./title";
+import { FilterCheckbox } from "./filter-checkbox";
+import { RangeSlider } from "./range-slider";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Фільтрування" size="sm" className="mb-5 font-bold" />
+
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можна збирати" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Ціна від і до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input type="number" min={100} max={1000} placeholder="5000" />
+        </div>
+
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+      </div>
+
+      <CheckboxFiltersGroup
+        className="mt-5"
+        title="Інгредієнти"
+        limit={6}
+        defaultItems={[
+          {
+            text: "Сирний соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Часник",
+            value: "3",
+          },
+          {
+            text: "Солоні огірочки",
+            value: "4",
+          },
+          {
+            text: "Червона цибуля",
+            value: "5",
+          },
+          {
+            text: "Томати",
+            value: "6",
+          },
+        ]}
+        items={[
+          {
+            text: "Сирний соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Часник",
+            value: "3",
+          },
+          {
+            text: "Солоні огірочки",
+            value: "4",
+          },
+          {
+            text: "Червона цибуля",
+            value: "5",
+          },
+          {
+            text: "Томати",
+            value: "6",
+          },
+          {
+            text: "Сирний соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Часник",
+            value: "3",
+          },
+          {
+            text: "Солоні огірочки",
+            value: "4",
+          },
+          {
+            text: "Червона цибуля",
+            value: "5",
+          },
+          {
+            text: "Томати",
+            value: "6",
+          },
+        ]}
+      />
+    </div>
+  );
+};
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,10 @@
 export { Categories } from "./categories";
-export { Header } from "./header";
+export { CheckboxFiltersGroup } from "./checkbox-filters-group";
 export { Container } from "./container";
+export { FilterCheckbox } from "./filter-checkbox";
+export { Filters } from "./filters";
+export { Header } from "./header";
+export { RangeSlider } from "./range-slider";
 export { SortPopup } from "./sort-popup";
 export { Title } from "./title";
 export { TopBar } from "./top-bar";
