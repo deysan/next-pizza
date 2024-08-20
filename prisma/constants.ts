1bca686dd814a2b1c9e35a571baac782ac7cf116
deysan <ideysan@gmail.com>
2024-08-20 23:30:07 +0300

    Create fake data

---
+++ prisma/constants.ts
@@ -0,0 +1,272 @@
+export const categories = [
+  {
+    name: "Пиццы",
+  },
+  {
+    name: "Закуски",
+  },
+  {
+    name: "Салати",
+  },
+  {
+    name: "Десерти",
+  },
+];
+
+export const ingredients = [
+  {
+    name: "Сир пармезан",
+    price: 70,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-1.webp",
+  },
+  {
+    name: "Сир моцарела",
+    price: 60,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-2.webp",
+  },
+  {
+    name: "Шинка",
+    price: 50,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-4.webp",
+  },
+  {
+    name: "Прошуто",
+    price: 70,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-3.webp",
+  },
+  {
+    name: "Гриби",
+    price: 27,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-5.webp",
+  },
+  {
+    name: "Помідори",
+    price: 25,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-6.webp",
+  },
+  {
+    name: "Перець болгарський",
+    price: 35,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-7.webp",
+  },
+  {
+    name: "Перець чилі",
+    price: 25,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-8.webp",
+  },
+  {
+    name: "Цибуля",
+    price: 15,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-9.webp",
+  },
+  {
+    name: "Оливки",
+    price: 50,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/olive-black.webp",
+  },
+  {
+    name: "Кукурудза",
+    price: 22,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-11.webp",
+  },
+  {
+    name: "Зелень",
+    price: 15,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-12.webp",
+  },
+  {
+    name: "Ананас",
+    price: 35,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-13.webp",
+  },
+  {
+    name: "Ковбаски баварські",
+    price: 40,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-23.webp",
+  },
+  {
+    name: "Пепероні",
+    price: 50,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-26.webp",
+  },
+  {
+    name: "Ковбаса салямі",
+    price: 50,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-24.webp",
+  },
+  {
+    name: "Куряче м’ясо",
+    price: 45,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-25.webp",
+  },
+  {
+    name: "Лосось",
+    price: 80,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-28.webp",
+  },
+  {
+    name: "Рукола",
+    price: 20,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-18.webp",
+  },
+  {
+    name: "Сир дорблю",
+    price: 60,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-29.webp",
+  },
+  {
+    name: "Сир фета",
+    price: 60,
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2021/07/ing-31.webp",
+  },
+].map((obj, index) => ({ id: index + 1, ...obj }));
+
+export const products = [
+  {
+    name: "Нагетси",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/nagetsy.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Картопля фрі",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2022/08/kartoplya-fri.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Картопля фрі Велика",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/04/velyka-fri.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Картопля фрі з допами",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2022/08/kartoplya-fri-z-dopamy.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Картопляні крокети",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2022/08/kartoplyani-krokety.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Сирні кульки з малиновим соусом",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2022/08/syrni-kulky-z-malynovym-sousom.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Креветки темпура з соусом тартар",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/10/krevetky-tartar.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Батат з соусом тартар",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/10/batat-tartar.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Сир Брі фрі з малиновим соусом",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/camambert-fri.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Сирний чедер",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2022/08/syrnyj.png",
+    categoryId: 2,
+  },
+  {
+    name: "Цезаріо",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/03/czezarnovyj.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "Сальмоне",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/03/salmonebez-rukoly.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "Ді Фета",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/03/feta.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "Ді Прошуто",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/03/proshutobez-rukoly.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "Рів'єра",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2023/10/rivyera1.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "Сіннабон з вишнею",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/sinn-cher.jpg",
+    categoryId: 4,
+  },
+  {
+    name: "Сіннабон шоколадний",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/sinn-choc.jpg",
+    categoryId: 4,
+  },
+  {
+    name: "Сіннабон з карамеллю",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/sinn-caram.jpg",
+    categoryId: 4,
+  },
+  {
+    name: "Нормальні горішки",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/gorishky.jpg",
+    categoryId: 4,
+  },
+  {
+    name: "Андрут",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/03/andrut-1.jpg",
+    categoryId: 4,
+  },
+  {
+    name: "Баскський чизкейк",
+    imageUrl:
+      "https://la.ua/vinnytsya/wp-content/uploads/sites/2/2024/05/photo_2024-02-28_16-23-37-1.jpg",
+    categoryId: 4,
+  },
+];
---
+++ prisma/seed.ts
@@ -0,0 +1,196 @@
+import { hashSync } from "bcrypt";
+
+import { Prisma } from "@prisma/client";
+
+import { categories, ingredients, products } from "./constants";
+import { prisma } from "./prisma-client";
+
+const randomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const generateProductItem = ({
+  productId,
+  pizzaType,
+  size,
+}: {
+  productId: number;
+  pizzaType?: 1 | 2;
+  size?: 20 | 30 | 40;
+}) => {
+  return {
+    productId,
+    price: randomNumber(190, 600),
+    pizzaType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "Іван Іванович",
+        email: "ivan@example.com",
+        password: hashSync("123456", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Адмін",
+        email: "admin@example.com",
+        password: hashSync("123456", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+
+  await prisma.category.createMany({
+    data: categories,
+  });
+
+  await prisma.ingredient.createMany({
+    data: ingredients,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  const pizza1 = await prisma.product.create({
+    data: {
+      name: "Українська",
+      imageUrl:
+        "https://veteranopizza.com/public/uploads/site_catalog/original_2e89917654d5499420e1841cc72fe61d.jpg",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(0, 5),
+      },
+    },
+  });
+
+  const pizza2 = await prisma.product.create({
+    data: {
+      name: "Вегетарiана",
+      imageUrl:
+        "https://veteranopizza.com/public/uploads/site_catalog/original_ee00c8e640a85a778ca4cba84b024ed6.jpg",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(5, 10),
+      },
+    },
+  });
+
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: "Пепероні",
+      imageUrl:
+        "https://veteranopizza.com/public/uploads/site_catalog/original_31df4042c5b3719a616a8ad40751e854.jpg",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(10, 40),
+      },
+    },
+  });
+
+  await prisma.productItem.createMany({
+    data: [
+      // Піца "Українська"
+      generateProductItem({ productId: pizza1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 40 }),
+
+      // Піца "Вегетарiана"
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 40 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 40 }),
+
+      // Піца "Пепероні"
+      generateProductItem({ productId: pizza3.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 40 }),
+
+      // Інші продукти
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 }),
+      generateProductItem({ productId: 18 }),
+      generateProductItem({ productId: 19 }),
+      generateProductItem({ productId: 20 }),
+      generateProductItem({ productId: 21 }),
+    ],
+  });
+
+  await prisma.cart.createMany({
+    data: [
+      {
+        userId: 1,
+        totalAmount: 0,
+        token: "111111",
+      },
+      {
+        userId: 2,
+        totalAmount: 0,
+        token: "222222",
+      },
+    ],
+  });
+
+  await prisma.cartItem.create({
+    data: {
+      productItemId: 1,
+      cartId: 1,
+      quantity: 2,
+      ingredients: {
+        connect: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      },
+    },
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
